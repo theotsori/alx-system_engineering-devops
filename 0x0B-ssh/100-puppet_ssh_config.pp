ec57a3af5976dc715f70b43bdb6175eff2de94c3
theotsori <theodoretsori@gmail.com>
2023-03-24 11:34:38 +0300

    Client configuration file (w/Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+file { "/root/.ssh":
+  ensure => "directory",
+  mode   => "0700",
+}
+
+file { '/root/.ssh/school':
+  ensure => 'file',
+  source => '/home/theodore/.ssh/school',
+  mode   => '0600',
+}
