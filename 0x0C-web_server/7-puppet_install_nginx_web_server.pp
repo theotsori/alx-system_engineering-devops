6811a1de94d35376111910f38a577ad4ae8d740e
theotsori <theodoretsori@gmail.com>
2023-03-30 11:32:02 +0300

    Install Nginx web server (w/Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,52 +1,5 @@
-# Install Nginx
-package { 'nginx':
-  ensure => installed,
-}
-
-# Configure Nginx to listen on port 80 and return "Hello World!"
-file { '/etc/nginx/sites-available/default':
-  ensure => file,
-  content => '
-    server {
-      listen 80;
-      location / {
-        return 200 "Hello World!";
-      }
-    }
-  ',
-  notify => Service['nginx'],
-}
-
-# Enable site configuration
-file { '/etc/nginx/sites-enabled/default':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
-}
-
-# Configure redirection
-file { '/etc/nginx/sites-available/redirect':
-  ensure => file,
-  content => '
-    server {
-      listen 80;
-      location /redirect_me {
-        return 301 /;
-      }
-    }
-  ',
-  notify => Service['nginx'],
-}
-
-# Enable redirection configuration
-file { '/etc/nginx/sites-enabled/redirect':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/redirect',
-  notify => Service['nginx'],
-}
-
-# Restart Nginx
-service { 'nginx':
-  ensure => running,
-  enable => true,
+# Install Nginx web server (w/ Puppet)
+exec { 'server configuration':
+  provider => shell,
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart'
 }
