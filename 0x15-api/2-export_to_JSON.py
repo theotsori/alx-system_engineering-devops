68bbe5745932eaa6ec71743bc028a1fb858c267f
theotsori <theodoretsori@gmail.com>
2023-04-25 09:06:22 +0300

    Export to JSON v1.3

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """
 Export data in the JSON format.
-
 Records all tasks that are owned by this employee.
 Format must be: { "USER_ID": [{"task": "TASK_TITLE",
                   "completed": TASK_COMPLETED_STATUS,
                   "username": "USERNAME"}, ... ]}
 File name must be: USER_ID.json
 """
-
 import json
 import requests
 import sys
-
 
 if __name__ == "__main__":
     # Get user id from command line argument
