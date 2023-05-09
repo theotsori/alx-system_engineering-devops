4d73b93d734fd399c755179bf3c900fa1f32668b
theotsori <theodoretsori@gmail.com>
2023-05-09 11:11:45 +0300

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Function that queries the Rddit api and
+prints the titles of the first 10 hot posts
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Top 10 hot posts"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()["data"]["children"]
+        for post in data:
+            print(post["data"]["title"])
+    else:
+        print(None)
