adbebb3616ffd2b9bcf99dbf0bc2aec5aac36718
theotsori <theodoretsori@gmail.com>
2023-05-10 12:29:24 +0300

    Recurse it! v1.2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,7 +9,7 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     """Recursive function"""
-    url = "https://www.reddit.com/r/{subreddit}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url,
