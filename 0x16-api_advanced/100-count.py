3d3f913dda010d06ef0d53819fd887522081f686
theotsori <theodoretsori@gmail.com>
2023-05-11 15:08:07 +0300

    ZEro to hero v1.2

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,7 +8,7 @@ delimited by spaces.
 import requests
 
 
-def count_words(subreddit, word_list, after='', word_count={}):
+def count_words(subreddit, word_list, after='', counts={}):
     """
     Queries the Reddit API, parses the title of all hot articles,
     and prints a sorted count of given keywords (case-insensitive,
@@ -22,30 +22,36 @@ def count_words(subreddit, word_list, after='', word_count={}):
     Returns:
       A list of tuples containing the keyword and the count of occurrences.
     """
-    if after is None:
-        sorted_words = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_words:
-            print(f"{word}: {count}")
+    if not word_list:
         return
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {"limit": 100, "after": after}
-    headers = {"User-Agent": "Mozilla/5.0"}
-
-    response = requests.get(url, headers=headers, params=params)
-
-    response.raise_for_status()
-
-    data = response.json()
-    posts = data["data"]["children"]
-    for post in posts:
-        title = post["data"]["title"]
+    
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    params = {'limit': 100, 'after': after}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+    
+    if response.status_code != 200:
+        return
+    
+    data = response.json()['data']
+    children = data['children']
+    after = data['after']
+    
+    for post in children:
+        title = post['data']['title'].lower()
         for word in word_list:
-            if word.lower() in title.lower() and not any(
-                prefix in title.lower() for prefix in ["java.",
-                                                       "java!",
-                                                       "java_"]
-            ):
-                word_count[word.lower()] = word_count.get(word.lower(), 0) + 1
-
-    count_words(subreddit, word_list, data["data"]["after"], word_count)
+            if ' ' + word.lower() + ' ' in title:
+                if word.lower() not in counts:
+                    counts[word.lower()] = 1
+                else:
+                    counts[word.lower()] += 1
+    
+    if after is not None:
+        count_words(subreddit, word_list, after, counts)
+    else:
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print('{}: {}'.format(word, count))
