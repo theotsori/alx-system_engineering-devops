fa66723e2e63b816be08f6b6a78a9f1a48f03159
theotsori <theodoretsori@gmail.com>
2023-05-17 08:47:17 +0300

    User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Change the OS configuration so that it is possible to login with
+# the holberton user and open a file without any error message.
+exec { 'change-os-configuration-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
