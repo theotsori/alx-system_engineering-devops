8a5901e67c331591d8f0e5ac9127b6afbc0923a8
theotsori <theodoretsori@gmail.com>
2023-05-19 10:03:10 +0300

    sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,3 +4,8 @@ exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
+
+exec {
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
